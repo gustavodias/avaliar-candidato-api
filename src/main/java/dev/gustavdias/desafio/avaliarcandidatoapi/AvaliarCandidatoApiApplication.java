d23ba9d8872844221ae24226c0955e2eda2bd1f5
gustavodias <gustavoemersonfd@gmail.com>
2022-01-21 16:19:55 -0300

    first commit

---
+++ src/main/java/dev/gustavdias/desafio/avaliarcandidatoapi/AvaliarCandidatoApiApplication.java
@@ -0,0 +1,13 @@
+package dev.gustavdias.desafio.avaliarcandidatoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliarCandidatoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliarCandidatoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/gustavdias/desafio/avaliarcandidatoapi/AvaliarCandidatoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gustavdias.desafio.avaliarcandidatoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliarCandidatoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
